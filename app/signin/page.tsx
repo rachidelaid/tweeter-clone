c3571e56a2028f14b968891f58a343a06e317b52
rachid el aid <r.elaid@void.fr>
2023-10-23 21:46:43 +0100

    adding signin

---
+++ app/signin/page.tsx
@@ -0,0 +1,21 @@
+import LoginForm from "@/app/components/widgets/LoginForm";
+import { getCsrfToken } from "next-auth/react";
+import Link from "next/link";
+
+const SignInPage = async () => {
+  const csrfToken = await getCsrfToken();
+
+  return (
+    <div className="flex flex-col h-screen w-screen justify-center items-center">
+      <LoginForm csrfToken={csrfToken} />
+      <p className="mt-2 text-xs text-gray-300">
+        you don&apos;t have an account ?{" "}
+        <Link href="/signup" className="text-primary hover:underline">
+          create one
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default SignInPage;
